7d39d8661632b7d49822e18da912f103ae6ff272
Vinod - PC <Vinod - PC@VINOD>
2016-11-21 07:56:54 +0530

    Add project files.

---
+++ SampleFileReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = File.ReadAllLines(@"C:\Users\Vinod - PC\Documents\ParkLoadData.csv")
+                           .Select(a => new Record(a.ToString()))
+                           .GroupBy(a => a.ID)
+                           .Select(a => new {
+                               id =a.Key,
+                               Duration = TimeSpan.FromTicks(a.Sum(r=>r.Duration.Ticks))
+                           });
+
+
+            using (var output = File.CreateText(@"C:\Users\Vinod - PC\Documents\summary.txt"))
+            {
+                foreach (var entry in summary)
+                {
+                    output.WriteLine($"{entry.id:D10} { entry.Duration:c}");
+                }
+            }
+                         
+                          
+                 
+        }
+    }
+}
---
+++ SampleFileReader/Record.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFileReader
+{
+    public class Record
+    {
+        public Record(string line)
+        {
+            this.Line = line;
+        }
+
+        public string Line { get; private set; }
+
+        public DateTime Start => DateTime.Parse(Line.Split(',')[0]);
+
+        public DateTime End => DateTime.Parse(Line.Split(',')[1]);
+
+        public long ID => Convert.ToInt64(Line.Split(',')[2]);
+
+        public TimeSpan Duration => End - Start;
+    }
+}
